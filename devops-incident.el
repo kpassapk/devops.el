777dd191829fcc73cf59656ebd11506cb75c2699
Kyle S Passarelli <kyle.passarelli@gmail.com>
2026-04-19 15:56:39 -0600

    Clean up project

--- devops-incident.el
+++
@@ -1,8 +0,0 @@
-;;;###autoload
-(defun devops-incident (slug)
-  "Create an incident worktree and notebook"
-  (interactive "sSlug:")
-  (let ((type "incident")
-	(stamp (devops--new-timestamp)))
-    (devops--create-worktree :type type :stamp stamp)))
-
